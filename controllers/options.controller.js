ef44418604db8252214fd7cf9669d1253cbbd1fb
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-08-26 01:38:54 +0700

    update options controllers

---
+++ controllers/options.controller.js
@@ -0,0 +1,62 @@
+const admin = require('firebase-admin');
+const firebaseHelper = require('firebase-functions-helper/dist');
+
+//Model
+
+const db = admin.firestore();
+const collectionName = 'options';
+
+//Get 1 option
+module.exports.getOption = (req, res) => {
+  const optiontId = req.params.optiontId;
+  firebaseHelper
+    .firestore
+    .getDocument(db, collectionName, optiontId)
+    .then(doc => res.status(200).send(doc))
+    .catch(err => res.status(400).send('Error'));
+};
+
+//Get all options
+module.exports.getOptions = async (req, res) => {
+  let data = [];
+  const optionsRef = db.collection(collectionName);
+  await optionsRef.get()
+    .then(snapshot => snapshot.forEach(doc => {
+      data.push(doc.data());
+    }))
+    .then(doc => res.status(200).send(data))
+    .catch(err => res.status(400).send('Error'));
+};
+
+//Create 1 option
+module.exports.createOption = (req, res) => {
+  let data = req.body;
+  
+  firebaseHelper
+    .firestore
+    .createDocumentWithID(db, collectionName, data.optionId, data)
+    .then(doc => res.status(201).send(`Create Option ${data.optionId} Successfully !`))
+    .catch(err => res.status(400).send('Error'));
+};
+
+//Update 1 option
+module.exports.updateOption = (req, res) => {
+  const optionId = req.params.optionId;
+  let data = req.body;
+
+  firebaseHelper
+    .firestore
+    .updateDocument(db, collectionName, optionId, data)
+    .then(doc => res.status(200).send(`Update option ${optionId} successfully !!!`))
+    .catch(err => res.status(400).send('Error'));
+};
+
+//Delete 1 option
+module.exports.deleteOption = (req, res) => {
+  const optionId = req.params.optionId;
+  firebaseHelper
+    .firestore
+    .deleteDocument(db, collectionName, optionId)
+    .then(doc => res.status(200).send(`Delete option ${optionId} successfully !!!`))
+    .catch(err => res.status(400).send('Error'));
+};
--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -10,22 +10,22 @@ const collectionName = 'products';
 module.exports.getProduct = (req, res) => {
   const productId = req.params.productId;
   firebaseHelper
-  .firestore
-  .getDocument(db, collectionName, productId)
-  .then(doc => res.status(200).send(doc))
-  .catch(err => res.status(400).send('Error'));
+    .firestore
+    .getDocument(db, collectionName, productId)
+    .then(doc => res.status(200).send(doc))
+    .catch(err => res.status(400).send('Error'));
 };
 
 //Get all product
 module.exports.getProducts = async (req, res) => {
   let data = [];
-  const productsRef = db.collection('products');
+  const productsRef = db.collection(collectionName);
   await productsRef.get()
-  .then(snapshot => snapshot.forEach(doc => {
-    data.push(doc.data());
-  }))
-  .then(doc => res.status(200).send(data))
-  .catch(err => res.status(400).send('Error'));
+    .then(snapshot => snapshot.forEach(doc => {
+      data.push(doc.data());
+    }))
+    .then(doc => res.status(200).send(data))
+    .catch(err => res.status(400).send('Error'));
 };
 
 //Create 1 product
@@ -36,17 +36,17 @@ module.exports.createProduct = (req, res) => {
   data['productStatus'] = 'OUTOFSTOCK';
 
   firebaseHelper
-  .firestore
-  .createNewDocument(db, collectionName, data)
-  .then(doc => {
-    data.productId = doc.id;
-    firebaseHelper
     .firestore
-    .updateDocument(db, collectionName, data.productId, data)
-    .then(doc => res.status(201).send(`Create Product ${data.productId} Successfully !`))
+    .createNewDocument(db, collectionName, data)
+    .then(doc => {
+      data.productId = doc.id;
+      firebaseHelper
+        .firestore
+        .updateDocument(db, collectionName, data.productId, data)
+        .then(doc => res.status(201).send(`Create Product ${data.productId} Successfully !`))
+        .catch(err => res.status(400).send('Error'));
+    })
     .catch(err => res.status(400).send('Error'));
-  })
-  .catch(err => res.status(400).send('Error'));
 };
 
 //Update 1 product
@@ -55,18 +55,18 @@ module.exports.updateProduct = (req, res) => {
   let data = req.body;
 
   firebaseHelper
-  .firestore
-  .updateDocument(db, collectionName, productId, data)
-  .then(doc => res.status(200).send(`Update product ${productId} successfully !!!`))
-  .catch(err => res.status(400).send('Error'));
+    .firestore
+    .updateDocument(db, collectionName, productId, data)
+    .then(doc => res.status(200).send(`Update product ${productId} successfully !!!`))
+    .catch(err => res.status(400).send('Error'));
 };
 
 //Delete 1 product
 module.exports.deleteProduct = (req, res) => {
   const productId = req.params.productId;
   firebaseHelper
-  .firestore
-  .deleteDocument(db, collectionName, productId)
-  .then(doc => res.status(200).send(`Delete product ${productId} successfully !!!`))
-  .catch(err => res.status(400).send('Error'));
+    .firestore
+    .deleteDocument(db, collectionName, productId)
+    .then(doc => res.status(200).send(`Delete product ${productId} successfully !!!`))
+    .catch(err => res.status(400).send('Error'));
 };
